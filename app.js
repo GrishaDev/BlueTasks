ae669402a1c02e543719e56b06cba452ee977506
grisha <codpure@gmail.com>
2019-10-24 11:01:25 +0300

    meh

--- app.js
+++ app.js
@@ -61,19 +61,29 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'client/BlueTasksNG')));
 app.use(express.static(path.join(__dirname, 'client/404page')));
 
+// app.use(express.static(path.join(__dirname, 'client')));
+
+
+
+// app.use('/users', usersRouter);
+
+
 app.use('/api', indexRouter);
-app.use('/users', usersRouter);
 app.use('/', pagesRouter);
 
+
+
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next)=> {
   next(createError(404));
 });
 
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next)=> {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
